e49ebea294c85091b0b68717abbf729c5997fe01
audreystott <audrey.stott@pawsey.org.au>
2021-04-06 17:20:15 +0800

    included sample download in ansible script

--- falcon/download-sample-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/cdunn2001/git-sym.git
-git clone https://github.com/pb-cdunn/FALCON-examples.git
-cd FALCON-examples
-../git-sym/git-sym update run/greg200k-sv2
-mv run/greg200k-sv2/data/greg200k-sv2/subreads* project_dir/ansible-falcon/falcon/
-rm -rf FALCON-examples/ git-sym/
