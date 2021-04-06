ed6410efdc69b26ea26bed094ae7cbe2a9e102e9
audreystott <audrey.stott@pawsey.org.au>
2021-04-06 17:08:28 +0800

    fixed dir path

--- falcon/download-sample-data.sh
+++ falcon/download-sample-data.sh
@@ -4,5 +4,5 @@ git clone https://github.com/cdunn2001/git-sym.git
 git clone https://github.com/pb-cdunn/FALCON-examples.git
 cd FALCON-examples
 ../git-sym/git-sym update run/greg200k-sv2
-mv run/greg200k-sv2/data/greg200k-sv2/subreads* project_dir
+mv run/greg200k-sv2/data/greg200k-sv2/subreads* project_dir/ansible-falcon/falcon/
 rm -rf FALCON-examples/ git-sym/
